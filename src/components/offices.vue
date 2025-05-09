9ab804528f457ea178f4f457b862e58f3e6de3ad
CheriseOtieno <cheriseotieno1707@gmail.com>
2025-05-09 15:42:34 +0300

    Update offices.vue

--- src/components/offices.vue
+++ src/components/offices.vue
@@ -29,36 +29,6 @@ Each office is equipped with storage facilities, delivery vehicles, and trained 
             alt="Offices"
             class="office-img"
             />
-            <div class="table-container">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>County</th>
-                            <th>Office Location</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="(office, index) in offices" :key="index">
-                            <td>{{ office.county }}</td>
-                            <td>{{ office.location }}</td>
-                        </tr>
-                    </tbody>
-                </table>
-                <button @click="editMode = !editMode">
-                    {{ editMode ? "Finish Editing" : "Edit Table" }}
-                </button>
-
-                <div v-if="editMode">
-                    <h4>Add New Office</h4>
-                    <input v-model="newCounty" placeholder="County" />
-                    <input v-model="newLocation" placeholder="Office Location" />
-                    <button @click="addOffice">Add</button>
-
-                    <h4>Delete Office (Index)</h4>
-                    <input type="number" v-model.number="deleteIndex" />
-                    <button @click="deleteOffice">Delete</button>
-                </div>
-            </div>
         </div>
     </main>
 </template>
@@ -99,79 +69,14 @@ Each office is equipped with storage facilities, delivery vehicles, and trained 
     }
 
     .office-img {
-        max-width: 400px;
+        width:100%;
         border-radius: 8px;
     }
-
-    .table-container {
-        flex: 1;
-        min-width: 300px;
-    }
-
-    table {
-        width: 100%;
-        border-collapse: collapse;
-        margin-bottom: 1rem;
-    }
-
-    th,
-    td {
-        border: 1px solid #ccc;
-        padding: 0.5rem;
-        text-align: left;
-    }
-
-    button {
-        margin-top: 0.5rem;
-    }
-
 
 </style>
 
 <script>
     export default {
-        data() {
-            return {
-                showModal: false,
-                editMode: false,
-                offices: [
-                    { county: 'Nairobi', location: 'Main HQ' },
-                    { county: 'Mombasa', location: 'Coastal Hub' },
-                ],
-                newCounty: '',
-                newLocation: '',
-                deleteIndex: null
-            };
-        },
-        methods: {
-            submitForm() {
-                alert("Form submitted!");
-                this.showModal = false;
-            },
-            addOffice() {
-                if (this.newCounty && this.newLocation) {
-                    this.offices.push({
-                        county: this.newCounty,
-                        location: this.newLocation
-                    });
-                    this.newCounty = '';
-                    this.newLocation = '';
-                }
-            },
-            deleteOffice() {
-                if (
-                    this.deleteIndex !== null &&
-                    this.deleteIndex >= 0 &&
-                    this.deleteIndex < this.offices.length
-                ) {
-                    this.offices.splice(this.deleteIndex, 1);
-                    this.deleteIndex = null;
-                } else {
-                    alert("Invalid index!");
-                }
-            }
-
-        }
-
+        name: 'offices'
     }
 </script>
